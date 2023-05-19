37439fae8341d2aeec004415ceae42dbb61cd770
Krishna Hendra <krisnahendra70@gmail.com>
2023-05-19 14:21:46 +0700

    feat: add store name

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -9,6 +9,45 @@ const loginWhatsapp = async (req, res) => {
   try {
     const { shop, code } = req.query;
 
+    const {
+      data: {
+        data: { accessToken },
+      },
+    } = await axios.get(
+      `${apiUrl}/integrations/api/shopify/BITLOGIN/auth/${shop}`
+    );
+
+    const {
+      data: {
+        shop: { name: shopName },
+      },
+    } = await axios.get(`https://${shop}/admin/api/2022-04/shop.json`, {
+      headers: {
+        "X-Shopify-Access-Token": accessToken,
+      },
+    });
+
+    const { data: barcodeData } = await axios.get(
+      `${apiUrl}/bitlogin/api/login/whatsapp/barcode/${shop}?code=${code}`
+    );
+
+    res.render("whatsappLogin", {
+      barcodeData,
+      shop,
+      apiUrl,
+      shopName,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: err.message,
+      detail: err,
+    });
+  }
+};
+
+const loginWhatsappWithDomain = async (req, res) => {
+  try {
     const { data: integration } = await axios.get(
       `${apiUrl}/bitlogin/api/integrations/shopify`,
       {
@@ -22,23 +61,16 @@ const loginWhatsapp = async (req, res) => {
         message: "Company integration not found",
       });
     }
-
     const companyId = integration.data[0].companyId;
     const {
       data: {
-        data: { name },
+        data: { name: company },
       },
     } = await axios.get(`${apiUrl}/system-login/company/${companyId}`);
 
-    const { data: barcodeData } = await axios.get(
-      `${apiUrl}/bitlogin/api/login/whatsapp/barcode/${shop}?code=${code}`
-    );
-
-    res.render("whatsappLogin", {
-      barcodeData,
-      shop,
-      apiUrl,
-      company: name,
+    res.status(200).json({
+      message: "Success",
+      company,
     });
   } catch (err) {
     res.status(500).json({
@@ -74,5 +106,6 @@ const loginSuccess = async (req, res) => {
 
 module.exports = {
   loginWhatsapp,
+  loginWhatsappWithDomain,
   loginSuccess,
 };
