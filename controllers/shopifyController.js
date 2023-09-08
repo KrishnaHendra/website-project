a6ae72f7b7f8c488e5f35867b5830f60e61c3bf7
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-09-08 22:11:37 +0700

    feat: create endpoint for update shopify customer

--- controllers/shopifyController.js
+++ controllers/shopifyController.js
@@ -1,1 +1,64 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const apiUrl = process.env.BACKEND_URL;
+
+const getShopifyCustomer = async (req, res) => {
+  res.status(200).json({
+    message: "OK",
+  });
+};
+
+const postShopifyCustomer = async (req, res) => {
+  try {
+    const { name, email, phone, domain } = req.body;
+
+    if (!name) {
+      return res.json({
+        success: false,
+        message: "Name is required!",
+      });
+    }
+    if (!email) {
+      return res.json({
+        success: false,
+        message: "Email is required!",
+      });
+    }
+    if (
+      !email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      return res.json({
+        success: false,
+        message: "Email format is incorrect!",
+      });
+    }
+
+    const { data } = await axios.post(
+      `${apiUrl}/bitlogin/api/shopify/customer`,
+      {
+        name,
+        email,
+        phone,
+        domain,
+      }
+    );
+
+    res.status(201).json(data);
+  } catch (err) {
+    res.status(500).json({
+      message: "Some error occured",
+      error: err.message,
+      detail: err,
+    });
+  }
+};
+
+module.exports = {
+  getShopifyCustomer,
+  postShopifyCustomer,
+};
