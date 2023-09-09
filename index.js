a46e6fd59dc017b35add955a8479c5c7a4a35d14
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-09-09 22:24:27 +0700

    feat: set default content

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const router = require("./routes");
 
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.get("/", (req, res) => {
-  const data = "<center><h1>Welcome to bitLogin!</h1></center>";
-  res.send(data);
-});
-
-const port = 3000;
-app.listen(port, (req, res) => {
-  console.log(`Server running on port ${port}`);
-});
