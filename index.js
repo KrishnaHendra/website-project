37f85fda8342d80cf973c8cef9fab1cf60d712b7
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-09-09 22:24:45 +0700

    feat: add cors and dotenv library

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require("./routes");
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(router);
+
+app.get("/", (req, res) => {
+  const data = "<center><h1>Welcome to bitLogin!</h1></center>";
+  res.send(data);
+});
+
+const port = process.env.PORT;
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}`);
+});
