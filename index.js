06d60162826a021044eddc8605ce7c79b070876f
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-09-09 22:23:57 +0700

    feat: remove cors

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const router = require("./routes");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(router);
 
